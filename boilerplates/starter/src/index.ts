dec4062aa8fe9a7240b850f5e1536dda60c0f3dd
Sylwester Mielniczuk <flaboy.com@gmail.com>
2023-09-14 19:45:18 +0100

    AudioContext - not found detection

--- boilerplates/starter/src/index.ts
+++ boilerplates/starter/src/index.ts
@@ -7,5 +7,50 @@
 
   */
 
+import _ from 'lodash';
+import Wad from 'web-audio-daw';
+
+//import * as Pizzicato from 'pizzicato';
+
+const array = [1, 2, 3, 4, 5];
+console.log(_.reverse(array));  // Should print [5, 4, 3, 2, 1]
+
+// const sound = new Pizzicato.Sound({
+//     source: 'wave',
+//     options: {
+//         type: 'sine',
+//         frequency: 440
+//     }
+// }, () => {
+//     sound.play();
+// });
+// console.log("Pizzicato")
+
+// let bell = new Wad({source : 'https://www.myserver.com/audio/bell.mp3'});
+// bell.play();
+// bell.stop();
+
+//console.log(Wad)
+
+// if (typeof AudioContext === 'undefined') {
+//     global.AudioContext = function() {
+//         // Mocked functions and properties
+//         this.createOscillator = () => {
+//             return {
+//                 start: () => {},
+//                 stop: () => {},
+//                 // ... other required methods and properties
+//             };
+//         };
+//         // ... other required methods and properties
+//     };
+// }
+
+if (typeof AudioContext !== 'undefined') {
+    console.log("AudioContext is available!");
+} else {
+    console.log("AudioContext is NOT available.");
+}
+
 
 console.log("index.ts");
---
+++ boilerplates/starter/src/types-overrides.d.ts
@@ -0,0 +1,6 @@
+/* types-overrides.d.ts */
+
+// declare module '@dcl/js-runtime' {
+//   // Override or extend types here. For example:
+//   type RequestRedirect = 'follow' | 'error' | 'manual' | 'my-new-type';
+// }
